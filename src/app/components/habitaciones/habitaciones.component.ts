125cde4fa049e5ddf4927cbfbab8ade19df58505
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-02-06 13:30:10 -0600

    cambios css

--- src/app/components/habitaciones/habitaciones.component.ts
+++ src/app/components/habitaciones/habitaciones.component.ts
@@ -1,14 +1,16 @@
+import { FiltroHotelesComponent } from './../filtro-hoteles/filtro-hoteles.component';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HabitacionesService } from '../../services/habitacion.service';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-habitaciones',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent, FiltroHotelesComponent],
   templateUrl: './habitaciones.component.html',
-  styleUrls: ['./habitaciones.component.css']
+  styleUrls: ['./habitaciones.component.css'],
 })
 export class HabitacionesComponent implements OnInit {
   hotelId!: number;
@@ -23,11 +25,11 @@ export class HabitacionesComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.hotelId = +params['hotelId'];
     });
 
-    this.route.queryParams.subscribe(queryParams => {
+    this.route.queryParams.subscribe((queryParams) => {
       this.fechaInicio = queryParams['fechaInicio'];
       this.fechaFin = queryParams['fechaFin'];
       this.huespedes = +queryParams['huespedes'];
@@ -41,7 +43,7 @@ export class HabitacionesComponent implements OnInit {
       hotelId: this.hotelId,
       fechaInicio: this.fechaInicio,
       fechaFin: this.fechaFin,
-      huespedes: this.huespedes
+      huespedes: this.huespedes,
     };
 
     this.habitacionesService.obtenerHabitaciones(filtros).subscribe((res) => {
