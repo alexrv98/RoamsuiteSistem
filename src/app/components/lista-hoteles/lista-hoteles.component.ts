adc12083173d915a6471a657a872cac77701ae37
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-02-06 12:12:34 -0600

    design

--- src/app/components/lista-hoteles/lista-hoteles.component.ts
+++ src/app/components/lista-hoteles/lista-hoteles.component.ts
@@ -3,30 +3,41 @@ import { ActivatedRoute } from '@angular/router';
 import { HotelService } from '../../services/hotel.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FiltroHotelesComponent } from '../filtro-hoteles/filtro-hoteles.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-lista-hoteles',
   templateUrl: './lista-hoteles.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, FiltroHotelesComponent, NavbarComponent],
   styleUrls: ['./lista-hoteles.component.css'],
 })
 export class ListaHotelesComponent implements OnInit {
   hoteles: any[] = [];
 
-  constructor(private route: ActivatedRoute, private hotelService: HotelService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private hotelService: HotelService
+  ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       const filtros = {
         destino: params['destino'],
         fechaInicio: params['fechaInicio'],
         fechaFin: params['fechaFin'],
-        huespedes: params['huespedes']
+        huespedes: params['huespedes'],
       };
 
-      this.hotelService.obtenerHotelesDisponibles(filtros).subscribe(response => {
-        this.hoteles = response.data;
-      });
+      this.hotelService
+        .obtenerHotelesDisponibles(filtros)
+        .subscribe((response) => {
+          this.hoteles = response.data;
+        });
     });
+  }
+  // Función para convertir el número de estrellas en un array de estrellas para mostrar en el HTML
+  getStarArray(promedio: number): number[] {
+    return Array(Math.round(promedio)).fill(1);
   }
 }
