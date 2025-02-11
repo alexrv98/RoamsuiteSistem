dc3aec52d357abe7e1fbfb92ed6c1aea9cf3f1aa
Jorgezs <ulilopezromero@gmail.com>
2025-02-11 17:48:09 +0100

    c

--- src/app/components/confirmacion-reserva/confirmacion-reserva.component.ts
+++ src/app/components/confirmacion-reserva/confirmacion-reserva.component.ts
@@ -26,16 +26,16 @@ export class ConfirmarReservaComponent implements OnInit {
     document.body.classList.remove('modal-open');
     document.querySelector('.modal-backdrop')?.remove();
     document.body.style.overflow = 'auto';
-
+  
     const state = history.state;
 
     if (state.reserva) {
       // Si ya hay una reserva almacenada, úsala en lugar de sobrescribirla.
       this.reserva = this.reserva || state.reserva;
-
+      
       // Si el cliente ya ha ingresado sus datos, no los borres.
       this.cliente = this.cliente.nombre ? this.cliente : { nombre: '', email: '', telefono: '' };
-
+  
       // Si el botón de PayPal aún no se ha cargado, lo cargamos
       if (!this.paypal) {
         this.cargarPaypalScript();
@@ -44,26 +44,29 @@ export class ConfirmarReservaComponent implements OnInit {
       this.router.navigate(['/']);
     }
   }
+<<<<<<< HEAD
+  
+  
+=======
 
-
-// Evento para detectar cuando el usuario intente abandonar la página
 @HostListener('window:beforeunload', ['$event'])
 
 unloadNotification($event: any): void {
   if (!this.pagoRealizado || !this.cliente.nombre || !this.cliente.email || !this.cliente.telefono) {
-    $event.returnValue = true;
+    $event.returnValue = true; 
   }
 }
 
+>>>>>>> 017e74ad6499d4b36448f2f173261329a024bd7d
   cargarPaypalScript() {
     if (window.paypal) {
       this.paypal = window.paypal;
       this.renderizarBotonPago();
       return;
     }
-
+  
     const scriptUrl = `https://www.paypal.com/sdk/js?client-id=AWIzDf7xorUxwwhL-i8PFdB4g4rO7r6y9quBVumXa8bllB86EiqsIXKtiPcCq8JqItGU1mWF0Xinoigs&components=buttons&currency=MXN&_=${new Date().getTime()}`;
-
+  
     const scriptElement = document.createElement("script");
     scriptElement.src = scriptUrl;
     scriptElement.onload = () => {
@@ -71,9 +74,9 @@ unloadNotification($event: any): void {
     };
     document.body.appendChild(scriptElement);
   }
-
-
-
+  
+  
+    
   renderizarBotonPago() {
     if (!document.getElementById('paypal-button-container')?.hasChildNodes()) {
       this.paypal.Buttons({
@@ -89,17 +92,17 @@ unloadNotification($event: any): void {
         onApprove: (data: any, actions: any) => {
           return actions.order.capture().then((details: any) => {
             alert('Pago realizado con éxito');
-            this.pagoRealizado = true;
+            this.pagoRealizado = true; 
           });
         },
         onError: (error: any) => {
           console.error("Error en el proceso de pago:", error);
           alert('Error al realizar el pago, intenta de nuevo.');
         }
-      }).render('#paypal-button-container');
+      }).render('#paypal-button-container');  
     }
   }
-
+    
   confirmarReserva() {
     if (this.pagoRealizado && this.cliente.nombre && this.cliente.email && this.cliente.telefono) {
       const datosReserva = {
@@ -127,5 +130,5 @@ unloadNotification($event: any): void {
       alert('Por favor, completa todos los campos y realiza el pago antes de confirmar.');
     }
   }
-
+  
 }
