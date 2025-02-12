4fcf9c8273e5f1e1d5a7625c1d879c825b818803
Alexadr7u7 <alex.lazcano.alexander@gmail.com>
2025-02-12 11:06:42 -0600

    uwu

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -8,8 +8,14 @@ import { NavbarComponent } from '../navbar/navbar.component';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  imports: [CommonModule, FormsModule, RouterModule, NavbarComponent],
-  styleUrls: ['./login.component.css']
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    NavbarComponent,
+    RouterLink,
+  ],
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   correo: string = '';
@@ -18,17 +24,17 @@ export class LoginComponent {
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  
-
   onLogin(): void {
     this.authService.login(this.correo, this.password).subscribe({
       next: (response) => {
         if (response.status === 'success') {
           const userRole = response.rol;
           const state = history.state;
-  
+
           if (state.reserva) {
-            this.router.navigate(['/confirmar-reserva'], { state: { reserva: state.reserva } });
+            this.router.navigate(['/confirmar-reserva'], {
+              state: { reserva: state.reserva },
+            });
           } else {
             this.router.navigate([userRole === 'admin' ? '/admin' : '/']);
           }
@@ -41,6 +47,4 @@ export class LoginComponent {
       },
     });
   }
-  
-  
 }
