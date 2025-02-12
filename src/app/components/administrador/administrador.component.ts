db449b6560c0257c556d3ba95799e351d26abe8c
Alexis <libravarillas777@gmail.com>
2025-02-12 17:11:21 -0600

    ok

--- src/app/components/administrador/administrador.component.ts
+++ src/app/components/administrador/administrador.component.ts
@@ -1,18 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { HotelService } from '../../services/hoteles.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-administrador',
-  imports: [],
   templateUrl: './administrador.component.html',
+  imports: [CommonModule, FormsModule],
   styleUrl: './administrador.component.css'
 })
-export class AdministradorComponent {
+export class AdministradorComponent implements OnInit {
+  hoteles: any[] = [];
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private hotelService: HotelService
+  ) {}
 
- logout(): void {
+  ngOnInit(): void {
+    this.cargarHoteles();
+  }
+
+  cargarHoteles(): void {
+    this.hotelService.obtenerHoteles().subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.hoteles = response.data;
+        } else {
+          console.error('Error al obtener hoteles:', response.message);
+        }
+      },
+      (error) => {
+        console.error('Error al cargar hoteles:', error);
+      }
+    );
+  }
+
+  logout(): void {
     this.authService.logout();
     console.log('Sesión cerrada correctamente');
     this.router.navigate(['/login']);
