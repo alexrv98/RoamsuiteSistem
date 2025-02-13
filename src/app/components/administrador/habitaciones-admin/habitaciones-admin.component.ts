617cfbffef3c2b1fda5dfb33f1cab77a7c91d015
Alexis <libravarillas777@gmail.com>
2025-02-13 10:48:00 -0600

    ok

--- src/app/components/administrador/habitaciones-admin/habitaciones-admin.component.ts
+++ src/app/components/administrador/habitaciones-admin/habitaciones-admin.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HabitacionService } from '../../../services/habitaciones.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-habitaciones-admin',
-  imports: [],
   templateUrl: './habitaciones-admin.component.html',
-  styleUrl: './habitaciones-admin.component.css'
+  imports:[CommonModule],
+  styleUrl: './habitaciones-admin.component.css',
 })
-export class HabitacionesAdminComponent {
+export class HabitacionesAdminComponent implements OnInit {
+  @Input() hotelId!: number;
+  habitaciones: any[] = [];
 
+  constructor(private habitacionService: HabitacionService) {}
+
+  ngOnInit(): void {
+    if (this.hotelId) {
+      this.cargarHabitaciones();
+    }
+  }
+
+  cargarHabitaciones(): void {
+    this.habitacionService.obtenerHabitaciones(this.hotelId).subscribe({
+      next: (response) => {
+        if (response.status === 'success') {
+          this.habitaciones = response.data;
+        } else {
+          console.error('Error al obtener habitaciones:', response.message);
+        }
+      },
+      error: (error) => {
+        console.error('Error al cargar habitaciones:', error);
+      },
+      complete: () => {
+        console.log('Carga de habitaciones completada');
+      }
+    });
+  }
+  
 }
